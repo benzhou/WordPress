90bee79de86b3bdac15400dbc6b147b1b0c97e87
Ben Zhou <amailtoaben@gmail.com>
2013-02-27 00:11:34 -0500

    new change to the plugin code base

---
+++ wp-content/plugins/digitalivy/digitalivy.php
@@ -0,0 +1,36 @@
+<?php 
+/*
+Plugin Name: Triton DigitalIvy Contest
+Plugin URI: http://www.tritondigital.com/publishers/audience-management/loyalty-social-engagement-gamification
+Description: DigitalIvy is cool
+Version: 0.1
+Author: Triton Digital -- Ben Zhou
+Author URI: http://tritondigital.com/
+License: GPL2
+*/
+
+
+/*  Copyright 2012  Triton Digital  (email : info@tritondigital.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+// Get Class File
+require_once "digitaliy_plugin.class.php";
+
+// init see plugin
+$diPlugin = new DigitalIvy_Plugin;
+// Admin UI
+add_action( "admin_menu" ,    array( $diPlugin, "adminUI" ) );
+
+?>
--- wp-content/plugins/digitalivy/digitalivy_plugin.class.php
+++ wp-content/plugins/digitalivy/digitalivy_plugin.class.php
@@ -1,5 +1,5 @@
 <?php 
-class DigitalIvy {
+class DigitalIvy_Plugin {
 	public function __construct() {
 
 		
