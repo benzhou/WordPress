31c83f251abbb59accc101bfc2f4d67795c0507c
Ben Zhou <amailtoaben@gmail.com>
2013-03-02 01:39:36 -0500

    Auto Release commit

--- wp-content/plugins/digitalivy/di_Admin_GeneralForm.class.php
+++ wp-content/plugins/digitalivy/di_Admin_GeneralForm.class.php
@@ -10,11 +10,12 @@ class DIAdminGeneralForm{
 	}
 
 	public function Get(){
-		include dirname(plugin_basename(__FILE__)) . '/templates/' . 'DI_Admin_GeneralForm.php';
+		//include dirname(plugin_basename(__FILE__)) . '/templates/DI_Admin_GeneralForm.php';
+		include plugin_dir_url(__FILE__).'/templates/DI_Admin_GeneralForm.php';
 	}
 
 	public function Post(){
-		include dirname(plugin_basename(__FILE__)) . '/templates/' . 'DI_Admin_GeneralForm.php';
+		include dirname(plugin_basename(__FILE__)) . '/templates/DI_Admin_GeneralForm.php';
 	}
 
 	public function Run(){
